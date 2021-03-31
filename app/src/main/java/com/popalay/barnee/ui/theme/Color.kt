57845a9d9607ad47a5fdcd6154387501548eb56d
popalay <popalay1@gmail.com>
2021-03-31 18:32:01 +0800

    Update typography Update colors

--- app/src/main/java/com/popalay/barnee/ui/theme/Color.kt
+++ app/src/main/java/com/popalay/barnee/ui/theme/Color.kt
@@ -2,8 +2,10 @@ package com.popalay.barnee.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Yellow = Color(0xFFF6A31E)
+val Yellow = Color(0xFFEEAF75)
 val DarkYellow = Color(0xFFF3C24A)
-val Grey = Color(0xFF0E0E0E)
+val Grey = Color(0xFF1C2126)
+val LightGrey = Color(0xFF939393)
+val Green = Color(0xFF4F9AA5)
 val Black = Color(0xFF000000)
 val White = Color(0xFFFFFFFF)
