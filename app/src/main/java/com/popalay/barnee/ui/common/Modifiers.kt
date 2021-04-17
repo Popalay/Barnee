9d5057508d60d256e36ab0ddd82ebf382985f378
popalay <popalay1@gmail.com>
2021-04-17 19:40:26 +0300

    Extract ShiftedLazyGrid widget

--- app/src/main/java/com/popalay/barnee/ui/common/Modifiers.kt
+++ app/src/main/java/com/popalay/barnee/ui/common/Modifiers.kt
@@ -1,11 +1,16 @@
 package com.popalay.barnee.ui.common
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 
 /**
@@ -18,4 +23,15 @@ fun Modifier.scrim(colors: List<Color>): Modifier = drawWithContent {
 
 fun Modifier.liftOnScroll(listState: LazyListState): Modifier = graphicsLayer {
     shadowElevation = if (listState.firstVisibleItemScrollOffset != 0) 1.dp.toPx() else 0F
+}
+
+@Composable
+
+operator fun PaddingValues.plus(another: PaddingValues) = with(LocalLayoutDirection.current) {
+    PaddingValues(
+        start = calculateStartPadding(this) + another.calculateStartPadding(this),
+        end = calculateEndPadding(this) + another.calculateEndPadding(this),
+        top = calculateTopPadding() + another.calculateTopPadding(),
+        bottom = calculateBottomPadding() + another.calculateBottomPadding(),
+    )
 }
