8180e5fa6dd370af99c5337f187c9e74c7cd7880
popalay <popalay1@gmail.com>
2021-04-20 19:42:36 +0300

    Interpret Uninitialized state as Loading

--- app/src/main/java/com/popalay/barnee/ui/util/ImageRequestExtensions.kt
+++ app/src/main/java/com/popalay/barnee/ui/util/ImageRequestExtensions.kt
@@ -1,10 +1,14 @@
 package com.popalay.barnee.ui.util
 
 import androidx.compose.ui.unit.IntSize
+import coil.memory.MemoryCache
 import coil.request.ImageRequest
 
 fun ImageRequest.Builder.applyForExtarnalImage(data: String, size: IntSize) =
     data(data + "?imwidth=${size.width}")
+        .memoryCacheKey(data)
+        .placeholderMemoryCacheKey(MemoryCache.Key(data))
+        .crossfade(true)
 
 fun ImageRequest.Builder.applyForInternalImage(data: String, size: IntSize) =
     data("https://res.cloudinary.com/barnee/image/upload/c_scale,w_${size.width}/$data")
