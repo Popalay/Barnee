346c47735aa5b708965ef22677549de630baaf7c
popalay <popalay1@gmail.com>
2021-04-20 19:42:36 +0300

    Interpret Uninitialized state as Loading

--- app/src/main/java/com/popalay/barnee/ui/common/StateLayout.kt
+++ app/src/main/java/com/popalay/barnee/ui/common/StateLayout.kt
@@ -5,6 +5,7 @@ import com.popalay.barnee.domain.Fail
 import com.popalay.barnee.domain.Loading
 import com.popalay.barnee.domain.Result
 import com.popalay.barnee.domain.Success
+import com.popalay.barnee.domain.Uninitialized
 import com.popalay.barnee.domain.isEmpty
 
 @Composable
@@ -16,7 +17,7 @@ fun <T : Any> StateLayout(
     content: @Composable (value: T) -> Unit
 ) {
     when {
-        value is Loading -> loadingState()
+        value is Uninitialized || value is Loading -> loadingState()
         value is Fail -> errorState()
         value.isEmpty -> emptyState()
         value is Success -> content(value())
