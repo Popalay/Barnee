b4502947edd284ab2ff224e24b902d2de0dad51a
popalay <popalay1@gmail.com>
2021-04-23 14:21:48 +0300

    Make each corner of squirce independant

--- app/src/main/java/com/popalay/barnee/ui/theme/Shape.kt
+++ app/src/main/java/com/popalay/barnee/ui/theme/Shape.kt
@@ -14,36 +14,43 @@ val Shapes = Shapes(
 
 val MediumSquircleShape = SquircleShape(0.25F)
 
+@Suppress("FunctionName")
+fun SquircleShape(@FloatRange(from = 0.0, to = 1.0) allCurve: Float) =
+    SquircleShape(allCurve, allCurve, allCurve, allCurve)
+
 /**
  * curve == 0 - rectangle
  * curve == 1 - ellipse
  */
 @Suppress("FunctionName")
-fun SquircleShape(@FloatRange(from = 0.0, to = 1.0) curve: Float) = GenericShape { size, _ ->
-    val oppositeCurve = 1 - curve
-
-    moveTo(x = 0F, y = size.height * curve)
+fun SquircleShape(
+    @FloatRange(from = 0.0, to = 1.0) curveTopLeft: Float,
+    @FloatRange(from = 0.0, to = 1.0) curveTopRight: Float,
+    @FloatRange(from = 0.0, to = 1.0) curveBottomLeft: Float,
+    @FloatRange(from = 0.0, to = 1.0) curveBottomRight: Float,
+) = GenericShape { size, _ ->
+    moveTo(x = 0F, y = size.height * curveTopLeft)
     cubicTo(
         x1 = 0F, y1 = 0F,
         x2 = 0F, y2 = 0F,
-        x3 = size.width * curve, y3 = 0F
+        x3 = size.width * curveTopLeft, y3 = 0F
     )
-    lineTo(x = size.width * oppositeCurve, y = 0F)
+    lineTo(x = size.width * (1 - curveTopRight), y = 0F)
     cubicTo(
         x1 = size.width, y1 = 0F,
         x2 = size.width, y2 = 0F,
-        x3 = size.width, y3 = size.height * curve
+        x3 = size.width, y3 = size.height * curveTopRight
     )
-    lineTo(x = size.width, y = size.height * oppositeCurve)
+    lineTo(x = size.width, y = size.height * (1 - curveBottomRight))
     cubicTo(
         x1 = size.width, y1 = size.height,
         x2 = size.width, y2 = size.height,
-        x3 = size.width * oppositeCurve, y3 = size.height
+        x3 = size.width * (1 - curveBottomRight), y3 = size.height
     )
-    lineTo(x = size.width * curve, y = size.height)
+    lineTo(x = size.width * curveBottomLeft, y = size.height)
     cubicTo(
         x1 = 0F, y1 = size.height,
         x2 = 0F, y2 = size.height,
-        x3 = 0F, y3 = size.height * oppositeCurve
+        x3 = 0F, y3 = size.height * (1 - curveBottomLeft)
     )
 }
