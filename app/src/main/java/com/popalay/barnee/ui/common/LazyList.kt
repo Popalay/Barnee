1cb6aace8c9867460350d3daf2061cebba9bd4f4
popalay <popalay1@gmail.com>
2021-04-27 19:21:42 +0300

    Update dependencies

---
+++ app/src/main/java/com/popalay/barnee/ui/common/LazyList.kt
@@ -0,0 +1,100 @@
+package com.popalay.barnee.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+const val DEFAULT_COLUMNS = 2
+val DefaultItemShift = 24.dp
+val DefaultHorizontalItemPadding = 24.dp
+val DefaultVerticalItemPadding = 0.dp
+
+fun LazyListScope.itemSpacer(height: Dp) {
+    item {
+        Spacer(Modifier.height(height).fillParentMaxWidth())
+    }
+}
+
+/**
+ * Displays a 'fake' grid using [LazyColumn]'s DSL. It's fake in that we just we add individual
+ * column items, with a inner fake row.
+ */
+fun <T> LazyListScope.itemsInGridIndexed(
+    items: List<T>,
+    columns: Int,
+    contentPadding: PaddingValues = PaddingValues(),
+    horizontalItemPadding: Dp = 0.dp,
+    verticalItemPadding: Dp = 0.dp,
+    itemContent: @Composable (index: Int, item: T) -> Unit
+) {
+    val rows = when {
+        items.size % columns == 0 -> items.size / columns
+        else -> (items.size / columns) + 1
+    }
+
+    for (row in 0 until rows) {
+        if (row == 0) itemSpacer(contentPadding.calculateTopPadding())
+
+        item {
+            val layoutDirection = LocalLayoutDirection.current
+
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = contentPadding.calculateStartPadding(layoutDirection),
+                        end = contentPadding.calculateEndPadding(layoutDirection)
+                    )
+            ) {
+                for (column in 0 until columns) {
+                    Box(modifier = Modifier.weight(1f)) {
+                        val index = (row * columns) + column
+                        if (index < items.size) {
+                            itemContent(index, items[index])
+                        }
+                    }
+                    if (column < columns - 1) {
+                        Spacer(modifier = Modifier.width(horizontalItemPadding))
+                    }
+                }
+            }
+        }
+
+        if (row < rows - 1) {
+            itemSpacer(verticalItemPadding)
+        } else {
+            itemSpacer(contentPadding.calculateBottomPadding())
+        }
+    }
+}
+
+fun <T> LazyListScope.itemsInGrid(
+    items: List<T>,
+    columns: Int,
+    contentPadding: PaddingValues = PaddingValues(),
+    horizontalItemPadding: Dp = 0.dp,
+    verticalItemPadding: Dp = 0.dp,
+    itemContent: @Composable (item: T) -> Unit
+) {
+    itemsInGridIndexed(
+        items,
+        columns,
+        contentPadding,
+        horizontalItemPadding,
+        verticalItemPadding
+    ) { _, item -> itemContent(item) }
+}
