dea584e708f90495e1f7a0897f271b49dbf7f88e
popalay <popalay1@gmail.com>
2021-06-24 15:28:00 +0300

    Bump version to 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 28
         targetSdk = 30
         versionCode = properties.getOrDefault("barnee.versioncode", 1).toString().toInt()
-        versionName = "1.0.6"
+        versionName = "1.0.7"
 
         signingConfigs {
             getByName("debug") {
