36ba8dcf2668cbe52f1d4ffeec4158cf7ae1d2f2
popalay <popalay1@gmail.com>
2021-07-23 21:02:30 +0300

    Bump version to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = properties.getOrDefault("barnee.versioncode", 1).toString().toInt()
-        versionName = "1.1.2"
+        versionName = "1.1.3"
 
         signingConfigs {
             getByName("debug") {
